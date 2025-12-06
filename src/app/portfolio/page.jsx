3970c0edd290716ed4bc5957e6fd0fcfb32bdc3b
rumonkh0 <rummankh0@gmail.com>
2025-12-06 23:48:14 +0600

    Navbar function

---
+++ src/app/portfolio/page.jsx
@@ -0,0 +1,15 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <section className="py-20 bg-white">
+        <div className="max-w-7xl mx-auto px-4">
+          <h1 className="text-4xl font-bold mb-4">About Us</h1>
+          <p className="text-lg text-gray-700">
+            We are a team of passionate individuals committed to delivering
+            exceptional digital solutions.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
